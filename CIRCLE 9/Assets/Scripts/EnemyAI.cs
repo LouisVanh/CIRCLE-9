58d0dc86fb0cb26c376e355df51a1a9f4cab7153
LouisVanhove <vanhovelouis123@gmail.com>
2023-10-31 23:08:03 +0100

    ENEMIES COME OUT OF THE ICE!
    and I randomised it to make it cleaner

--- CIRCLE 9/Assets/Scripts/EnemyAI.cs
+++ CIRCLE 9/Assets/Scripts/EnemyAI.cs
@@ -28,21 +28,25 @@ public class EnemyAI : MonoBehaviour
     private float lerpedValue;
     private float duration = 3;
     private float totalScale = 1;
+    private bool isLerping;
+    public bool isDead;
     private void Start()
     {
         player = GameObject.Find("PLAYER").transform;
         agent = GetComponent<NavMeshAgent>();
         _enabled = false;
         agent.enabled = false;
+        lerpedValue = this.transform.position.y;
     }
     IEnumerator LerpValue(float start, float end)
     {
         float timeElapsed = 0;
         float reducedRange = Mathf.Abs(start - end);
         float reducedDuration = duration * (reducedRange / totalScale);
-
-        while (timeElapsed < reducedDuration)
+        reducedDuration += Random.Range(0, 5);
+        while (timeElapsed < reducedDuration && !isDead)
         {
+            isLerping = true;
             float t = timeElapsed / reducedDuration;
 
             lerpedValue = Mathf.Lerp(start, end, t);
@@ -51,7 +55,19 @@ public class EnemyAI : MonoBehaviour
             yield return null;
         }
         lerpedValue = end;
+        isLerping = false;
         _shouldEnable = true;
+    }
+    private void EnableWhenNeeded()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+            StartCoroutine(LerpValue(this.transform.position.y, 1.05f));
+
+        if (!_enabled &&  _shouldEnable && !isDead)
+        {
+            this.gameObject.GetComponent<NavMeshAgent>().enabled = true;
+            _enabled = true;
+        }
     }
     private void Update()
     {
@@ -64,19 +80,10 @@ public class EnemyAI : MonoBehaviour
             if (!playerInSightRange && !playerInAttackRange) Patroling();
             if (playerInSightRange && !playerInAttackRange) ChasePlayer();
             if (playerInAttackRange) AttackPlayer();
-        } else
+        } else if (!agent.enabled & isLerping)
         {
             //lerp above the ice
             this.transform.position = new Vector3(this.transform.position.x, lerpedValue, this.transform.position.z);
-        }
-    }
-    private void EnableWhenNeeded()
-    {
-        if (!_enabled &&  _shouldEnable)
-        {
-            this.gameObject.GetComponent<NavMeshAgent>().enabled = true;
-            _enabled = true;
-            StartCoroutine(LerpValue(lerpedValue, 1));
         }
     }
     private void Patroling()
--- CIRCLE 9/Assets/Scripts/Shotgun.cs
+++ CIRCLE 9/Assets/Scripts/Shotgun.cs
@@ -76,6 +76,7 @@ public class Shotgun : MonoBehaviour
                 {
                     var rb = hit.transform.gameObject.AddComponent<Rigidbody>();
                     hit.transform.gameObject.GetComponent<NavMeshAgent>().enabled = false;
+                    hit.transform.gameObject.GetComponent<EnemyAI>().isDead = true;
                     var distanceBetweenEnemyAndPlayer = Vector3.Distance(Camera.main.transform.position, hit.point);
                     rb.AddForce(Camera.main.transform.forward.normalized * _bulletKnockback / distanceBetweenEnemyAndPlayer);
                 }
