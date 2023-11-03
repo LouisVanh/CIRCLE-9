56fac15728f0aec57bc6f7b6be99460687e1a092
LouisVanhove <vanhovelouis123@gmail.com>
2023-11-03 09:48:42 +0100

    Acheron scene fleshed out
    needs models + sounds, but most coding is done (including camerafade)

--- CIRCLE 9/Assets/Scripts/Boat.cs
+++ CIRCLE 9/Assets/Scripts/Boat.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using CameraFading;
 
 public class Boat : MonoBehaviour
 {
@@ -15,11 +16,14 @@ public class Boat : MonoBehaviour
     [SerializeField] private AudioClip _voiceLines;
     [SerializeField] private AudioSource _acheronAudioSource;
 
+    private float _timer;
+    [SerializeField] private float _voiceLineLength;
+
 
     void Start()
     {
         _enemyWaveSystem = GameObject.Find("EnemyWaveSystem");
-        _playerControls = GameObject.Find("EnemyWaveSystem");
+        _playerControls = GameObject.Find("PLAYER");
 
         _enemyWaveSystem.SetActive(false);
         _playerControls.SetActive(false);
@@ -31,14 +35,22 @@ public class Boat : MonoBehaviour
     {
         _boat.position += new Vector3(_boatSpeed * Time.deltaTime, 0, 0);
         _boat.position = new Vector3(_boat.position.x, Mathf.Sin(Time.timeSinceLevelLoad) * _boatBobSpeed , _boat.position.z);
+        _timer += Time.deltaTime;
+        if(_timer > _voiceLineLength)
+        {
+            _timer = 0;
+            SwitchCameras();
+        }
     }
 
     private void SwitchCameras()
     {
-        _introPlayerCam.gameObject.SetActive(false);
-        // code here to activate player
-        _playerControls.SetActive(true);
-        _enemyWaveSystem.SetActive(true);
-
+        CameraFade.Out(() => // short notation for a callback after 2 seconds
+        {
+            _introPlayerCam.gameObject.SetActive(false);
+            // code here to activate player
+            _playerControls.SetActive(true);
+            _enemyWaveSystem.SetActive(true);
+        }, 4f);
     }
 }
