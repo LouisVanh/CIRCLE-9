6242c52b923bbd2d8db743732ed2f1a43a7b2e1b
LouisVanhove <vanhovelouis123@gmail.com>
2023-11-04 21:46:08 +0100

    Gate Test, Skulls on gate

--- CIRCLE 9/Assets/Scripts/Gate.cs
+++ CIRCLE 9/Assets/Scripts/Gate.cs
@@ -6,12 +6,32 @@ public class Gate : MonoBehaviour
 {
     [SerializeField] private PlayerBehaviour _player;
     [SerializeField] private int _amountOfSkulls = 5;
+    [SerializeField] private GameObject _skull;
+    [SerializeField] private Transform _pos1;
+    [SerializeField] private Transform _pos2;
+    [SerializeField] private Transform _pos3;
+    [SerializeField] private Transform _pos4;
+    [SerializeField] private Transform _pos5;
+    [SerializeField] private Transform _pos6;
+    [SerializeField] private List<Transform> _positions;
+
+
+
     void Start()
     {
-        
+        _positions.Add(_pos1);
+        _positions.Add(_pos2);
+        _positions.Add(_pos3);
+        _positions.Add(_pos4);
+        _positions.Add(_pos5);
+        _positions.Add(_pos6);
+
     }
 
-
+    public void SpawnSkullOnGatePosition(int i)
+    {
+        Instantiate(_skull, _positions[i].position, Quaternion.identity);
+    }
     void Update()
     {
         
